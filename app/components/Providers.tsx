f839cad259c465810a894bef21a5191719e7bb64
tejasvi raj <tejasviraj8686@gmail.com>
2024-03-02 10:45:30 +0530

    everything should now work

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -9,10 +9,8 @@ export default function CartProvider({ children }: { children: ReactNode }) {
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-      successUrl="https://localhost:3000/stripe/success"
-      cancelUrl="https://localhost:3000/stripe/error"
-      // successUrl="https://commerce-next-yt.vercel.app/stripe/success"
-      // cancelUrl="https://commerce-next-yt.vercel.app/stripe/error"
+      successUrl="https://tejasvi-nextjs.vercel.app/stripe/success"
+      cancelUrl="https://tejasvi-nextjs.vercel.app/stripe/error"
       currency="USD"
       billingAddressCollection={false}
       shouldPersist={true}
